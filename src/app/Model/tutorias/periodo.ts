be00d97290a27542279381a1df17994878581938
PsicomonTamer <veemonfly@yahoo.com>
2022-05-20 21:53:01 -0500

    2222

--- src/app/Model/tutorias/periodo.ts
+++ src/app/Model/tutorias/periodo.ts
@@ -49,34 +49,4 @@ export class Registro {
     comportamiento: Number = 0;
     id_matricula: Number = 0;
     id_asignatura: Number = 0;
-
-}
-
-export class Estudiante {
-    apellido: String=""
-    cedula: String=""
-    nombre:String=""
-    id_registro: Number = 0;
-    aporte1: Number = 0;
-    aporte2: Number = 0;
-    aporte3: Number = 0;
-    aporte4: Number = 0;
-    evaluacion1: Number = 0;
-    aporte5: Number = 0;
-    aporte6: Number = 0;
-    aporte7: Number = 0;
-    aporte8: Number = 0;
-    evaluacion2: Number = 0;
-    examenfinal: Number = 0;
-    promediofinal: Number = 0;
-    examen_supletorio: Number = 0;
-    promedio_supletorio: Number = 0;
-    examen_remedial: Number = 0;
-    promedio_remedial: Number = 0;
-    examen_gracia: Number = 0;
-    promedio_gracia: Number = 0;
-    comportamiento: Number = 0;
-    id_matricula: Number = 0;
-    id_asignatura: Number = 0;
-    
 }
